1b056644f42cd6916aed6ffaf60e67106ad5a720
Fernando Fazio Sinigaglia <fernandosini@MacBook-Air-de-Fernando.local>
2021-07-05 00:03:18 -0300

    adicionando as rotas de match usuario e o controller do usuario

---
+++ routes/match.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router();
+
+
+//criando as rotas do match
+
+router.get("/api/matches/:userId")
+
+
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -8,3 +8,8 @@ router.get("/api/users")
 router.get("/api/users/gender/find/different/:gender")
 router.get("/api/users/update/:userId")
 router.get("/api/users/delete/:userId")
+router.get("/api/users/avatar/:userId")
+router.get("/api/users/photos/:userId")
+
+
+module.exports = router;
