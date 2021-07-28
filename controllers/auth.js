e44d3519121100e2e07088289d06ae562fd9b050
Fernando Fazio Sinigaglia <fernandosini@MacBook-Air-de-Fernando.local>
2021-07-27 21:22:24 -0300

    login funcionando arrumei parentesis errado

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,9 +7,9 @@ const expressJwt = require("express-jwt")
 exports.register = async (req, res) => {
 
     const emailExists = await User.findOne({ email: req.body.email })
-    const userNameExists = await User.findOne({username: req.body.username})
+    const userNameExists = await User.findOne({ username: req.body.username })
 
-    if (emailExists||userNameExists) {
+    if (emailExists || userNameExists) {
         return res.status(403).json({
             error: "User already exists because this email or username is in use"
         })
@@ -17,17 +17,17 @@ exports.register = async (req, res) => {
 
     const newUser = await new User(req.body);
     await newUser.save();
-    return res.status(200).json( { message: "User registered successfully! Please make Login!" })
+    return res.status.json({ message: "User registered successfully! Please make Login!" })
 }
 
 exports.login = (req, res) => {
 
-    const { emailUser, password } = req.body;
-    User.findOne({ email: emailUser }, (err, user) => {
+    const { email, password } = req.body;
+    User.findOne({ email: email }, (err, user) => {
         if (err || !user) {
             return res.status(401).json({ error: "User with this email not found. Please register!" })
         }
-    })
+    
 
     if (!user.authenticate(password)) {
         return res.status(401).json({ error: "Email and password not found" })
@@ -35,9 +35,10 @@ exports.login = (req, res) => {
 
     const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET)
     res.cookie("token", token, { expire: new Date() + 9999 });
+    
     const { _id, username, firstname, lastname, birthday, gender, roleUser, email, avatar_profile, about, usertype, job, livesin, school, company, photos, role } = user;
-
-    return res.json({ token, user: { _id, username, firstname, lastname, birthday, gender, roleUser, email, avatar_profile, about, usertype, job, livesin, school, company, photos, role } })
+    return res.json({user: { _id, username, firstname, lastname, birthday, gender, roleUser, email, avatar_profile, about, usertype, job, livesin, school, company, photos, role },token })
+})
 }
 exports.requireLogin = expressJwt({
     secret: process.env.JWT_SECRET,
